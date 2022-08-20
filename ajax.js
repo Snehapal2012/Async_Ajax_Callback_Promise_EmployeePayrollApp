f5e3145b76400be7b2e4bea200c9831167204f53
snehapal2012 <snehapal2012@gmail.com>
2022-08-20 17:14:15 +0530

    Modified: view data from JSON server using Ajax & Promise

--- ajax.js
+++ ajax.js
@@ -3,12 +3,16 @@ let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 function makeAJAXCall(methodType, url, callback, async = true, data = null) {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200 || xhr.status === 201) {
-                callback(xhr.responseText);
-            } else if (xhr.status >= 400) {
-                console.log("Handle 400 Client Error or 500 Server Error");
-            }
+        console.log("State Changed Called. Ready State: " + xhr.readyState
+        + "Status: " + xhr.status);
+        if(xhr.status.toString().match('^[2][0-9]{2}$')) {
+            console.log(xhr.responseText);
+        }else if(xhr.status.toString().match('^[4,5][0-9]{2}$')){
+            reject({
+                status: xhr.status,
+                statusText: xhr.statusText
+            });
+            console.log("XHR Failed");
         }
     }
     xhr.open(methodType, url, async);
@@ -19,21 +23,21 @@ function makeAJAXCall(methodType, url, callback, async = true, data = null) {
     } else xhr.send();
     console.log(methodType + " request sent to the server");
 }
-const getURL = "http://127.0.0.1:3000/employees/";
+const getURL = "http://127.0.0.1:3000/employee/";
 
 function getUserDetails(data) {
     console.log("Get User Data " + data);
 }
 makeAJAXCall("GET", getURL, getUserDetails);
 
-const deleteURL = "http://localhost:3000/employees/3";
+const deleteURL = "http://localhost:3000/employee/3";
 
 function userDeleted(data) {
     console.log("User Deleted " + data);
 }
 makeAJAXCall("DELETE", deleteURL, userDeleted, false);
 
-const postURL = "http://localhost:3000/employees/";
+const postURL = "http://localhost:3000/employee/";
 const emplData = { "name": "Sneha", "salary": "800000" };
 
 function userAdded(data) {
